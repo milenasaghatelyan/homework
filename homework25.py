3c4f7912fffb5d4d1b25813295e859b467794d09
milenasaghatelyan <121639639+milenasaghatelyan@users.noreply.github.com>
2022-12-29 11:58:50 -0800

    Add files via upload

---
+++ homework25.py
@@ -0,0 +1,27 @@
+# # k={'0':0,'1':1,'2':(2,4,6,8),'3':(1,3,7,9),'4':(4,6),'5':5,'6':6,'7':(1,3,7,9),'8':(2,4,6,8),'9':(1,9)}
+#
+# def func(x,y):
+#     x,y=str(x),str(y)
+#     x=x[(len(x) - 1)]
+#     y=y[(len(y)-1)]
+#     a=int(x)**int(y)
+#     print(str(a)[-1])
+#
+#
+# func(929764567897545679,3345678766786788766)
+
+
+
+
+# input = ["aba", "aa", "z", "ad", "vcd", "aba"] #O(n)
+# output=[]
+# maximal=len(max(input,key=len))
+# def function(h):
+#     for i in h:
+#         a=len(i)
+#         if a==maximal:
+#             output.append(i)
+#     print(output)
+# function(input)
+
+
